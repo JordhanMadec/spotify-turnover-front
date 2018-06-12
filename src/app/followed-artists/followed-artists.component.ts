b104dec3ab42d741562e73528939d636ba6cf20c
Jordhan <jordhan.madec@gmail.com>
2018-06-12 23:32:53 +0200

    Create followed artists page

--- src/app/followed-artists/followed-artists.component.ts
+++ src/app/followed-artists/followed-artists.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowedArtistsComponent implements OnInit {
 
+  private followedArtists = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   constructor() { }
 
   ngOnInit() {
